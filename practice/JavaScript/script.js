b5535b8987405f59bd000a72f0cd0067b850337f
TuttoShin <starprickle29@gmail.com>
2026-03-19 19:48:59 +0900

    자바스크립트 심화 실습1

--- practice/JavaScript/script.js
+++ practice/JavaScript/script.js
@@ -9,17 +9,51 @@ const countEl = document.querySelector('#todo-count') // 개수 표시
 console.log(input)
 console.log(list)
 
-// CREATE: 새 li 요소 만들기 (아직 화면에 없음)
-const newItem = document.createElement('Li')
+btn.addEventListener('click', function() {
+    const text = input.value; 
+    if (text.length === 0) {
+        alert('할 일을 입력해주세요')
+        return
+    }
 
-// 내용 채우기 - 지금은 텍스트 직접 입력
-newItem.textContent = '자바스크립트 공부하기'
+    // CREATE: 새 li 요소 만들기 (아직 화면에 없음)
+    const newItem = document.createElement('Li')
 
-// 아직 화면에 안 나타남! console로 확인만
-console.log(newItem)
+    // 내용 채우기 - 지금은 텍스트 직접 입력
+    newItem.textContent = text
 
-// APPEND: 만든 요소를 목록에 붙이기
-list.append(newItem)
+    // 아직 화면에 안 나타남! console로 확인만
+    console.log(newItem)
+
+    // APPEND: 만든 요소를 목록에 붙이기
+    list.append(newItem)
+
+    //입력창 초기화
+    input.value = ''
+})
 
 // 이제 화면에 나타남
 
+// --------------------------------------------------------
+
+// 데이터 타입 기본형 예시
+const age = 20
+const name = '아기사자'
+
+// 데이터 타입 참조형 예시
+const galaxy = {
+    brand: 'Samsung',
+    battery: 80
+}
+
+galaxy.brand
+galaxy.battery
+
+// 기본형끼리 묶어서 객체 생성
+const ewhain = {
+    name: '아기사자',
+    age: 20
+}
+
+ewhain.name
+ewhain.age
