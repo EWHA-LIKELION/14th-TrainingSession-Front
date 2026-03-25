0f4c9d6e45f5abf87ba1e565bdc17657d111b239
TuttoShin <starprickle29@gmail.com>
2026-03-25 12:47:00 +0900

    React 입문 과제 제출

---
+++ project_react_vite/src/pages/post-detail/PostDetailPage.jsx
@@ -0,0 +1,15 @@
+import PageHeader from './components/PageHeader'; 
+import PostArticle from './PostArticle'; 
+import CommentSection from './CommentSection'; 
+
+function PostDetailPage() {
+  return (
+    <div>
+      <PageHeader />
+      <PostArticle />
+      <CommentSection />
+    </div>
+  );
+}
+
+export default PostDetailPage;
