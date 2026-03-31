720dcef8f833a03be52041bf0a5c97135f7fecd8
HaHuiseo <huiseoha425@ewha.ac.kr>
2026-04-01 00:22:35 +0900

    Hooks&Routing

---
+++ project_react_vite/src/pages/post-detail/PostDetailPage.jsx
@@ -0,0 +1,18 @@
+import { useParams } from "react-router-dom";
+import PageHeader from "../../components/PageHeader";
+import PostArticle from "./PostArticle";
+import CommentSection from "./CommentSection";
+
+function PostDetailPage() {
+  const { id } = useParams();
+  return (
+    <>
+      <PageHeader />
+      <main>
+        <PostArticle id={id} />
+        <CommentSection />
+      </main>
+    </>
+  );
+}
+export default PostDetailPage;
