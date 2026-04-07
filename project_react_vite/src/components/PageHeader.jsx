302b30f8bdee1c2977b1076f4d0ab9883889f207
TuttoShin <starprickle29@gmail.com>
2026-04-08 00:01:25 +0900

    Tailwind CSS 과제제출

--- project_react_vite/src/components/PageHeader.jsx
+++ project_react_vite/src/components/PageHeader.jsx
@@ -1,8 +1,19 @@
 const PageHeader = () => {
   return (
-    <header className="flex bg-main-1 w-full text-black text-6.25 p-4">
-      <img src="/icons/back.svg" alt="back" />
-      <h1>목록으로</h1>
+    // fixed: 상단 고정, w-full: 가로 꽉 차게, z-10: 다른 요소들보다 위에 배치
+    // y/pl/pr: 패딩
+    // flex items-center: 내부 요소들을 가로로 배치하고 수직 중앙 정렬
+    <header className="fixed top-0 w-full z-10 h-[65px] bg-white py-[15px] pl-66 pr-66.5 flex items-center gap-2">
+      <img
+        src="/icons/back.svg"
+        alt="back"
+        className="w-6 h-6 cursor-pointer"
+      />
+
+      {/* font-normal은 글자 두께!! */}
+      <h1 className="text-xl font-normal text-black cursor-pointer">
+        목록으로
+      </h1>
     </header>
   );
 };
--- project_react_vite/src/pages/post-detail/CommentItem.jsx
+++ project_react_vite/src/pages/post-detail/CommentItem.jsx
@@ -1,29 +1,46 @@
-const CommentItem = ({ 
-    author = "작성자", 
-    date = "0000. 00. 00. 00:00", 
-    content = "댓글 내용",
-    isMyComment = Boolean,
- }) => {
-    return (
-        <li>
-            <header>
-            <div>
-                <img src="/images/profile.png" alt="profile" />
-                <div>
-                    <h5>{author}</h5>
-                    <p>{date}</p>
-                </div>
-            </div>
-            {isMyComment ? (
-                <div>   
-                    <button>수정</button>
-                    <button>삭제</button>
-                </div>
-            ) : null}
-        </header>
-        <p>{content}</p>
+const CommentItem = ({
+  author = "작성자",
+  date = "0000. 00. 00. 00:00",
+  content = "댓글 내용",
+  isMyComment = Boolean,
+  showDivider = true,
+}) => {
+  return (
+    <li className="flex flex-col items-start gap-3 p-5 self-stretch">
+      <header className="flex justify-between items-center self-stretch">
+        <div className="flex items-center gap-2.5">
+          <img
+            src="/images/profile.png"
+            alt="profile"
+            className="w-10 h-10 rounded-full"
+          />
+          <div className="flex flex-col items-start justify-center">
+            <h5 className="text-base font-semibold leading-5 text-black">
+              {author}
+            </h5>
+            <p className="text-xs font-medium leading-4 text-gray-2">{date}</p>
+          </div>
+        </div>
+        {isMyComment ? (
+          <div className="flex gap-2">
+            <button className="text-sm font-semibold leading-none text-gray-1 text-center">
+              수정
+            </button>
+            <button className="text-sm font-semibold leading-none text-error text-center">
+              삭제
+            </button>
+          </div>
+        ) : null}
+      </header>
+      <p className="ml-12 mb-3 text-base font-normal leading-5 text-black self-stretch">
+        {content}
+      </p>
+
+      {showDivider && (
+        <div className="w-[380px] h-[1px] bg-gray-3 my-2.5 self-center"></div>
+      )}
     </li>
-    );
+  );
 };
 
 export default CommentItem;
