fcb33cae48bed3389f87875d998b7fa902427d91
TuttoShin <starprickle29@gmail.com>
2026-04-07 20:24:54 +0900

    Tailwind CSS 실습

--- project_react_vite/src/components/PageHeader.jsx
+++ project_react_vite/src/components/PageHeader.jsx
@@ -1,16 +1,10 @@
 const PageHeader = () => {
-    const style = {
-        display: "flex",
-        backgroundColor: "green",
-        color: "white",
-        fontSize: "25",
-    };
-    return (
-        <header style={style}>
-            <img src="/icons/back.svg" alt="back" />
-            <h1>목록으로</h1>
-        </header>
-    );
+  return (
+    <header className="flex bg-main-1 w-full text-black text-6.25 p-4">
+      <img src="/icons/back.svg" alt="back" />
+      <h1>목록으로</h1>
+    </header>
+  );
 };
 
 export default PageHeader;
