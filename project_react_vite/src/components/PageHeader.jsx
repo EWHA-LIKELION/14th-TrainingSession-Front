31d5b0d9a7a873bc104ea144099fdcd9bc2eb64d
ChoiYeongjin <aliennue@ewhain.net>
2026-04-09 00:49:42 +0900

    Tailwind CSS +

--- project_react_vite/src/components/PageHeader.jsx
+++ project_react_vite/src/components/PageHeader.jsx
@@ -2,7 +2,7 @@ const PageHeader = () => {
    
     const title = "목록으로";
     return (
-        <header className="flex items-center justify-center w-full h-[4.0625rem] px-[16.4375rem] py-[0.9375rem] bg-white">
+        <header className="flex items-center justify-center w-full h-[4.0625rem] py-[0.9375rem] bg-white">
             <div className="flex items-center w-[46.9375rem]">
                 <div className="flex items-center justify-between w-[6.6875rem] h-[2.1875rem] shrink-0">
                     <img src="/icons/back.svg" alt="back" className="w-6 h-6 aspect-square shrink-0"/>
