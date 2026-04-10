92127bc3b541babb7ceeba3fcec18960d7d7fbd3
Seo yerin <yerins.cse@ewha.ac.kr>
2026-04-10 15:42:33 +0900

    Tailwind CSS 실습 코드 (#51)
    * PR 템플릿 추가 (#2)
    
    * ✨html&css 심화: 실습 및 과제 코드 (#1)
    
    * ✨html&css 심화: 실습 및 과제 코드
    
    * Update practice/HTML & CSS/index.html
    
    * ♻️ 바닐라 프로젝트 과제용 이미지 아이콘 변경
    
    * ✨ photo 이미지 추가
    
    * 🚚 icons -> images 폴더명 변경
    
    * ✨ profile 이미지 추가
    
    * 🚚 디렉토리명 & 수정
    
    * Fix grid link in index.html
    
    * design: tailwind css 실습 코드
    
    ---------
    
    Co-authored-by: Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
    Co-authored-by: Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>

--- project_react_vite/src/pages/post-detail/CommentItem.jsx
+++ project_react_vite/src/pages/post-detail/CommentItem.jsx
@@ -1,22 +1,32 @@
 const CommentItem = ({ author, date, content, isMyComment }) => {
   return (
-    <li>
-      <header>
-        <div>
-          <img src="/images/profile.png" alt="profile" />
-          <div>
-            <h5>{author}</h5>
-            <p>{date}</p>
+    <li className="flex w-175 flex-col items-start gap-2.5 bg-white p-5">
+      <header className="flex w-full justify-between">
+        <div className="flex flex-row items-center gap-2">
+          <img
+            src="/images/profile.png"
+            alt="profile"
+            className="aspect-square h-11.25 w-11.25"
+          />
+          <div className="flex flex-col items-start gap-1">
+            <h5 className="text-center text-base font-semibold text-black">
+              {author}
+            </h5>
+            <p className="text-gray-2 text-xs font-medium">{date}</p>
           </div>
         </div>
         {isMyComment ? (
-          <div>
-            <button>수정</button>
-            <button>삭제</button>
+          <div className="flex items-start gap-2">
+            <button className="text-gray-1 text-center text-sm font-medium">
+              수정
+            </button>
+            <button className="text-error text-center text-sm font-medium">
+              삭제
+            </button>
           </div>
         ) : null}
       </header>
-      <p>{content}</p>
+      <p className="pl-13 text-base font-normal text-black">{content}</p>
     </li>
   );
 };
--- project_react_vite/src/pages/post-detail/CommentSection.jsx
+++ project_react_vite/src/pages/post-detail/CommentSection.jsx
@@ -28,15 +28,22 @@ const CommentSection = () => {
   }, [comment]);
 
   return (
-    <section>
-      <div>
+    <section className="flex w-187.75 flex-col items-start gap-7 rounded-lg bg-white p-7.5">
+      <div className="flex gap-1 text-2xl font-semibold text-black">
         댓글
-        <span>2</span>
+        <span className="text-gray-1 text-2xl font-semibold">2</span>
       </div>
 
-      <form action="submit_url">
-        <div>
-          <img src="/images/profile.png" alt="profile" />
+      <form
+        action="submit_url"
+        className="flex flex-col items-end justify-center gap-5 self-stretch"
+      >
+        <div className="flex items-start gap-3 self-stretch">
+          <img
+            src="/images/profile.png"
+            alt="profile"
+            className="aspect-square h-11.25 w-11.25 rounded-[45px]"
+          />
           <textarea
             value={comment}
             // onChange={(e) => {
@@ -45,12 +52,21 @@ const CommentSection = () => {
             onChange={(e) => setComment(e.target.value)}
             placeholder="댓글을 입력하세요."
             required
+            className="placeholder-gray-2 border-gray-2 flex w-158.5 items-center self-stretch rounded-lg border bg-white px-4 py-3"
           ></textarea>
-          <div>{comment.length} / 100</div>
         </div>
-        <button disabled={comment.length === 0} type="submit">
-          댓글 작성
-        </button>
+        <div className="flex w-full items-center justify-between">
+          <div className="text-gray-1 flex pl-13 text-base font-medium">
+            {comment.length} / 100
+          </div>
+          <button
+            disabled={comment.length === 0}
+            type="submit"
+            className="bg-gray-2 flex items-center justify-center rounded-lg px-5 py-1.5 text-center text-base font-semibold text-white"
+          >
+            댓글 작성
+          </button>
+        </div>
       </form>
 
       {/* 1단계: props 개념 설명 - CommentItem을 직접 두 번 사용 */}
@@ -70,7 +86,7 @@ const CommentSection = () => {
       </ul> */}
 
       {/* 2단계: map으로 리팩토링 */}
-      <ul>
+      <ul className="h flex flex-col gap-2">
         {comments.map((comment) => (
           <CommentItem
             key={comment.id}
