2ea79d11518f2508dbd726e195bf794783256e9a
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-05-06 18:14:09 +0900

    ✨ Toast 컴포넌트 및 ToastManager 추가

---
+++ project_react_vite/src/components/Toast.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useEffect } from "react";
+
+/**
+ * Toast 컴포넌트
+ */
+
+const Toast = ({ type = "check", text = "" }) => {
+  return (
+    <div
+      className={`flex w-95 items-center gap-2 rounded-lg px-5 py-3 ${type === "check" ? "bg-[#00BC7D]/80" : "bg-[#FF5558]/80"}`}
+    >
+      {type === "check" ? (
+        <img src="/icons/check.svg" />
+      ) : (
+        <img src="/icons/alert.svg" />
+      )}
+      <p className="text-sm font-semibold text-white">{text}</p>
+    </div>
+  );
+};
+
+/**
+ * ToastManager 컴포넌트
+ */
+
+export const ToastManager = ({
+  type,
+  text,
+  isOpen,
+  duration = 3000,
+  onClose,
+}) => {
+  const [visible, setVisible] = React.useState(false);
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    setVisible(true);
+
+    const timer = setTimeout(() => {
+      setVisible(false);
+      setTimeout(() => onClose?.(), 200);
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [isOpen, duration, onClose]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed top-24 right-7 z-50 -translate-x-1/2">
+      <div
+        className={`transition-all duration-300 ease-out ${visible ? "translate-y-0 opacity-100" : "translate-y-2 opacity-0"} `}
+      >
+        <Toast type={type} text={text} />
+      </div>
+    </div>
+  );
+};
+
+export default Toast;
