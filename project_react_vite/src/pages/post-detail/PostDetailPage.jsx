3bc2d41504855a313a85039851ed240079b766ea
Lee Hyewon <2491027@ewha.ac.kr>
2026-05-27 21:38:09 +0900

    TanStack Query & 배포

--- project_react_vite/src/pages/post-detail/PostDetailPage.jsx
+++ project_react_vite/src/pages/post-detail/PostDetailPage.jsx
@@ -1,34 +1,25 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import PageHeader from "../../components/PageHeader";
 import PostArticle from "./PostArticle";
 import CommentSection from "./CommentSection";
-import { useEffect, useState } from "react";
 import api from "../../api";
 
 function PostDetailPage() {
   const { id } = useParams();
 
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get(`/blog/${id}`);
-
-        console.log(response.data);
-
-        setPost(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchPosts();
-  }, [id]);
+  const { data: post } = useQuery({
+    queryKey: ["post", id],
+    queryFn: async () => {
+      const response = await api.get(`/blog/${id}`);
+      return response.data;
+    },
+  });
 
   return (
     <>
       <PageHeader />
+
       <main className="mx-auto flex max-w-[811px] flex-col gap-10 px-[30px] pt-[45px] pb-[30px]">
         <PostArticle post={post} />
         <CommentSection comments={post?.comments} />
