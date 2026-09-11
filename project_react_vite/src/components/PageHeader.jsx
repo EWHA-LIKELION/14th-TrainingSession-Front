77f5508392153180bc812b1781eae92789bc7884
TuttoShin <starprickle29@gmail.com>
2026-09-11 15:00:04 +0900

    Vite Assets 경로 관리 실습 및 과제

--- project_react_vite/src/components/PageHeader.jsx
+++ project_react_vite/src/components/PageHeader.jsx
@@ -1,14 +1,12 @@
+import backIcon from "@/assets/icons/back.svg";
+
 const PageHeader = () => {
   return (
     // fixed: 상단 고정, w-full: 가로 꽉 차게, z-10: 다른 요소들보다 위에 배치
     // y/pl/pr: 패딩
     // flex items-center: 내부 요소들을 가로로 배치하고 수직 중앙 정렬
-    <header className="fixed top-0 w-full z-10 h-[65px] bg-white py-[15px] pl-66 pr-66.5 flex items-center gap-2">
-      <img
-        src="/icons/back.svg"
-        alt="back"
-        className="w-6 h-6 cursor-pointer"
-      />
+    <header className="fixed top-0 w-full z-10 h-16.25 bg-white py-3.75 pl-66 pr-66.5 flex items-center gap-2">
+      <img src={backIcon} alt="back" className="w-6 h-6 cursor-pointer" />
 
       {/* font-normal은 글자 두께!! */}
       <h1 className="text-xl font-normal text-black cursor-pointer">
