450ef3678090df7aaf5892cc6df43dce01f6aeea
ChoiYeongjin <aliennue@ewhain.net>
2026-09-13 02:04:01 +0900

    Vite assets & 경로 관리

--- project_react_vite/src/pages/post-detail/PostDetailPage.tsx
+++ project_react_vite/src/pages/post-detail/PostDetailPage.tsx
@@ -1,34 +1,13 @@
-import type { Post } from "../../types/post";
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import PageHeader from "../../components/PageHeader";
+import PageHeader from "@/components/PageHeader";
 import PostArticle from "./PostArticle";
 import CommentSection from "./CommentSection";
 
 import { useQuery } from "@tanstack/react-query";
-import api from "../../api";
+import api from "@/api";
 
 function PostDetailPage() {
   const { id } = useParams();
-
-  // 처음엔 데이터가 없으니 null, 받아오면 Post 가 들어옴 → Post | null
-  const [post, setPost] = useState<Post | null>(null);
-
-  useEffect(() => {
-    if (!id) return; // id 가 없으면 요청하지 않음 (간단한 방어)
-
-    const fetchPost = async () => {
-      try {
-        const data = await getPost(id);
-
-        setPost(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchPost();
-  }, [id]);
 
   const { data: post } = useQuery({
     queryKey: ["post", id],
